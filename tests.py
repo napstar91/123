cc049ded0299f564410d2c43cf1465f975f70c48
Borislav <123732395+napstar91@users.noreply.github.com>
2023-01-29 23:21:41 +0000

    Delete tests.py

--- tests.py
+++
@@ -1,3 +0,0 @@
-height = 119
-    output = run_io_fun("{}\n".format(height), rollercoaster)
-    assert "Sorry, you're not tall enough to go on the ride." in output, f"Expected 'Sorry, you're not tall enough to go on the ride.', but got {output}"
